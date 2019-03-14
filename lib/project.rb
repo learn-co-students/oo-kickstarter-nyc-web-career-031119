b1d25c1e86ff4176ae94078d9420201e81f9a212
Xavier Downing <xaviernational@aol.com>
2019-03-14 15:31:03 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_reader :title
+  attr_accessor :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  def add_backer(backer_instance)
+    @backers << backer_instance
+    # backer_instance.back_project(self)
+    backer_instance.backed_projects << self
+
+  end
+end
